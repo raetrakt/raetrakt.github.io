f41d9ba6f0eb3ef069d21edd846440af04a90fd2
Fabian Pitzer <fabian.pitzer@gmx.de>
2019-09-26 20:05:32 +0200

    windspiel_project initial commit

---
+++ windspiel/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
+smooth();
+	noFill();
+
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+var a = 0;
+
+function draw() {
+	background(255);
+	rotate(a, [0, 1, 1]);
+
+	ellipse(0, 0, height / 2, height / 2, 50);
+	rotateX(HALF_PI);
+	ellipse(0, 0, height / 2, height / 2, 50);
+	rotateY(HALF_PI);
+	ellipse(0, 0, height / 2, height / 2, 50);
+	a += PI / 120;
+
+}
