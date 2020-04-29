adba3d9ef00130808943f8d2c29ed0308886d870
Fabian Pitzer <fabian.pitzer@gmx.de>
2020-04-29 14:01:26 +0200

    added galleries with scroll bug iframe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //preview images
 document.querySelectorAll(".preview").forEach(function(el) {
     el.addEventListener('mouseenter', function() {
@@ -31,6 +30,78 @@ document.querySelectorAll(".clickable").forEach(function(el) {
     })
 })
 
+
 //removing the margin bottom from the last project item here
 //because it didnt work in css with :last-child
-document.querySelector(".project-media").lastElementChild.style.marginBottom = "0px";
+if (document.querySelector(".project-media") != null) {
+    document.querySelector(".project-media").lastElementChild.style.marginBottom = "0px";
+}
+
+
+//write current year in copyright
+if (document.querySelector(".copyright") != null) {
+    let date = new Date();
+    document.querySelector(".copyright").innerHTML = "<p>&copy; Fabian Pitzer " 
+    + date.getFullYear() + "</p>";
+}
+
+
+
+//horizontal scrolling for galleries
+if (document.querySelector(".images") != null) {
+
+    let images = document.querySelector(".images");
+    let scrollInput = 0;
+    let easing = .18;
+    let scrollSpeed = 150;
+
+    //get scroll input
+    window.addEventListener('wheel', function(e) {
+        e.preventDefault();
+        if (e.deltaY > 0) scrollInput += scrollSpeed;
+        else scrollInput -= scrollSpeed;
+
+      }, { passive: false });
+
+    //execute scrolling every x with easing
+    setInterval(
+        function scroll() {
+            let easedScroll = (scrollInput * easing);
+            images.scrollLeft += easedScroll;
+            scrollInput -= easedScroll;
+            scrollInput = parseInt(scrollInput); //whithout this line it floats when scrolling back
+               
+        }
+    , 10);
+
+    //fix for not being able to scroll over iframe
+    var iframe = document.querySelector('iframe');
+
+    iframe.contentWindow.addEventListener('wheel', event => {
+        console.log("test");
+        
+        iframe.parent.document.dispatchEvent(new WheelEvent('wheel', event));
+    });
+}
+
+
+//else layout breaks on resize
+window.onresize = function(){ location.reload(); }
+
+
+//gallery video fix aspect ratio
+let videos = document.querySelectorAll(".image-container iframe");
+videos.forEach(function(vid) {
+    let vidHeight = vid.clientHeight;
+    let vidWidth = vidHeight/9*16 + "px";
+    vid.style.width = vidWidth;
+});
+
+
+
+
+
+
+
+
+
