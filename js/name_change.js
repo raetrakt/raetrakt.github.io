cbd1ac97911e910cb684308d5a4f94cfc37d0647
raetrakt <fabian.pitzer@gmx.de>
2025-03-09 17:13:14 +0100

    add name transition to title and trigger automatically

--- js/name_change.js
+++ js/name_change.js
@@ -1,30 +1,34 @@
-const nameElem = document.querySelector(".name-button");
-const nameElemText = document.querySelector(".name-text");
+const nameElem = document.querySelector('.name-button');
+const nameElemText = document.querySelector('.name-text');
 const nameStates = [
-  "FABIAN PITZER",
-  "FABIA PITZER",
-  "FABI PITZER",
-  "FAB PITZER",
-  "FA PITZER",
-  "F PITZER",
-  " PITZER",
-  "PITZER",
-  "PITZER.",
-  "PITZER.X",
-  "PITZER.XY",
-  "PITZER.XYZ",
+  'FABIAN PITZER',
+  'FABIA PITZER',
+  'FABI PITZER',
+  'FAB PITZER',
+  'FA PITZER',
+  'F PITZER',
+  ' PITZER',
+  'PITZER',
+  'PITZER.',
+  'PITZER.X',
+  'PITZER.XY',
+  'PITZER.XYZ',
 ];
 
 let transitionTimeout;
 let isReverting = false;
 let currentTransitionIndex = nameStates.length - 1;
 let transitionStepDuration = 90; // Amount of time the change of one letter should take in ms
+let timeUntilAutoChange = 15000;
+let timeSpentOnFullName = 3000;
 
 function startTransition() {
   clearTimeout(transitionTimeout);
 
   function updateState() {
     nameElemText.innerText = nameStates[currentTransitionIndex];
+    // Add current state of transition to HTML title
+    document.title = nameStates[currentTransitionIndex];
 
     if (!isReverting) {
       if (currentTransitionIndex < nameStates.length - 1) {
@@ -43,14 +47,24 @@ function startTransition() {
 }
 
 // Add hover functionality on desktop
-if (!window.matchMedia("(max-width: 767px)").matches) {
-  nameElem.addEventListener("mouseenter", () => {
+if (!window.matchMedia('(max-width: 767px)').matches) {
+  nameElem.addEventListener('mouseenter', () => {
     isReverting = true;
     startTransition();
   });
 
-  nameElem.addEventListener("mouseleave", () => {
+  nameElem.addEventListener('mouseleave', () => {
     isReverting = false;
     startTransition();
   });
 }
+
+// Trigger Transition automatically after delay
+setInterval(() => {
+  isReverting = true;
+  startTransition();
+  setTimeout(() => {
+    isReverting = false;
+    startTransition();
+  }, timeSpentOnFullName);
+}, timeUntilAutoChange); // Time of overall loop
